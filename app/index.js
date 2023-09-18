7c55eb2017508fffa9f5caa019b74599f061b2a1
muratozbk <slipstream11@yandex.com>
2023-09-18 23:23:45 +0300

    setup project

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router'
+import { COLORS, icons, images, SIZES } from '../constants'
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home 1</Text>
+        </View>
+    )
+}
+
+export default Home;
+
+// 25:26
